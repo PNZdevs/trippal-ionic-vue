2d1f612e841579cc8f9ba2166fc1101f6fced74e
Andrea Pozza <andrea.pozza@hotmail.com>
2021-03-06 00:42:13 +0000

    feat: add fonts.css

--- src/main.ts
+++ src/main.ts
@@ -23,6 +23,7 @@ import '@ionic/vue/css/display.css';
 /* Theme variables */
 import './theme/variables.css';
 import './theme/custom-colors.css'
+import './theme/fonts.css'
 
 const app = createApp(App)
   .use(IonicVue)
