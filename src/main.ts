9c24c10a894f386efa567b02a9ffe9460d3cb874
Andrea Pozza <andrea.pozza@hotmail.com>
2021-03-09 15:22:49 +0000

    feat: add custom-style.scss

--- src/main.ts
+++ src/main.ts
@@ -24,6 +24,7 @@ import '@ionic/vue/css/display.css';
 import './theme/variables.css';
 import './theme/custom-colors.css'
 import './theme/fonts.css'
+import './theme/custom-style.scss'
 
 const app = createApp(App)
   .use(IonicVue)
