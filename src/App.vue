cb9c65432d39c69210a9b9eef3a2fe74beb0aefe
Andrea Pozza <andrea.pozza@hotmail.com>
2021-03-21 15:38:08 +0100

    feat: handle backbutton behaviour

--- src/App.vue
+++ src/App.vue
@@ -7,12 +7,23 @@
 <script lang="ts">
 import { IonApp, IonRouterOutlet } from '@ionic/vue';
 import { defineComponent } from 'vue';
+import { Plugins } from '@capacitor/core';
 
 export default defineComponent({
   name: 'App',
   components: {
     IonApp,
     IonRouterOutlet
+  },
+  mounted() {
+    const { App } = Plugins
+    App.addListener('backButton', () => {      
+      const isModalPresent = Boolean(document.querySelector('ion-modal'))
+      const canGoBack = Boolean(this.$router.options.history.state.back || isModalPresent)
+      if(!canGoBack) App.exitApp()
+    })
+
   }
+  
 });
 </script>
